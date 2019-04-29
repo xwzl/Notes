8b676ca284fbdac9ebacb9afd24ee89e6cc4427a
徐伟智 <624244232@qq.com>
2019-04-30 00:01:59 +0800

    shiro

---
+++ spring-boot/src/main/java/com/java/boot/system/aop/ControllerAop2.java
@@ -0,0 +1,27 @@
+package com.java.boot.system.aop;
+
+/**
+ * @author xuweizhi
+ * @date 2018/11/13 16:38
+ */
+//@Aspect
+//@Component
+//@Order(3)
+//@Slf4j
+public class ControllerAop2 {
+
+    /*//@Pointcut("execution(public * com.web.pro.controller.*.*(..))")
+    public void pointCut() {
+    }
+
+    //@Before("pointCut()")
+    public void before(JoinPoint joinPoint) {
+        log.info("Before Advice={}", "前置通知2");
+    }
+
+    //@After("pointCut()")
+    public void after() {
+        log.info("After Advice={}", "2");
+    }
+*/
+}
---
+++ spring-boot/src/main/java/com/java/boot/system/aop/MapperAop.java
@@ -0,0 +1,51 @@
+package com.java.boot.system.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xuweizhi
+ * @date 2018/11/15 8:59
+ */
+@Component
+@Aspect
+@Order(3)
+@Slf4j
+public class MapperAop {
+
+    /**
+     * 保证每个线程都有一个单独的实例
+     */
+    private ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    private boolean flag = false;
+
+    /**
+     * 横切所有的cont方法
+     */
+    @Pointcut("execution(* com.graduation.design.mapper.*.*(..))")
+    public void pointcut() {
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) throws NoSuchMethodException {
+        threadLocal.set(System.currentTimeMillis());
+        //UrlControllerAop.otherLogPrint(joinPoint);
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getInterfaces()[0].getSimpleName();
+        String methodName =  joinPoint.getSignature().getName() + "  耗时" + ((System.currentTimeMillis() - threadLocal.get())) + "ms";
+        log.info("Mapper接口名称:{} 执行方法:{}", className,methodName);
+        threadLocal.remove();
+    }
+
+}
