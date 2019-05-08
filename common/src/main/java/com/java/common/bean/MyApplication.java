a4f18443fe18bd02204a295bbd06f2bcd37156e4
徐伟智 <624244232@qq.com>
2019-05-08 08:52:36 +0800

    Linux

---
+++ common/src/main/java/com/java/common/bean/MyApplication.java
@@ -0,0 +1,16 @@
+package com.java.common.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuweizhi
+ * @since 2019/05/07 9:49
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
