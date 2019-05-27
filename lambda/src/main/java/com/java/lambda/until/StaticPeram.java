e49efe53a2c75e0ffcefbeb57cf773c9fa513065
徐伟智 <624244232@qq.com>
2019-05-27 18:04:21 +0800

    11

--- lambda/src/main/java/com/java/lambda/until/StaticPeram.java
+++ lambda/src/main/java/com/java/lambda/until/StaticPeram.java
@@ -15,7 +15,7 @@ public class StaticPeram {
     final static String accessKeyId = "LTAIxxgebxDvvNUm";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
     final static String accessKeySecret = "HJSpZxPUUvnj1YiuetnzCnpSThhsxs";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
     // 必填:短信签名-可在短信控制台中找到
-    final static String SignName = "骑士人才系统啊";// 阿里云配置你自己的短信签名填入
+    final static String SignName = "骑士人才系统";// 阿里云配置你自己的短信签名填入
     // 必填:短信模板-可在短信控制台中找到
     final static String TemplateCode = "SMS_151835412"; // 阿里云配置你自己的短信模板填入
  }
