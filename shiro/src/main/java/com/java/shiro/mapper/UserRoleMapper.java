4ce5627451732a5f9b91f02661822e529bf19703
xuweizhi <158262751>
2019-07-12 10:41:06 +0800

    小改动

--- shiro/src/main/java/com/java/shiro/mapper/UserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.java.shiro.mapper;
-
-import com.java.shiro.model.UserRole;
-
-/**
- * UserRoleDAO继承基类
- */
-public interface UserRoleMapper extends MyBatisBaseMapper<UserRole, UserRole> {
-}
--- spring-boot/README.md
+++
@@ -1,3 +0,0 @@
-彩色日志打印 加虚拟机参数
-
--Dlog4j.skipJansi=false
