9d8e977ad361d819b0050e898937a13c3e5c4695
xuweizhi <158262751>
2019-07-29 17:12:13 +0800

    'thread'

--- thread/src/main/java/geym/conc/ch6/completable/CFutureMain7.java
+++ thread/src/main/java/geym/conc/ch6/completable/CFutureMain7.java
@@ -1,32 +1,29 @@
 package geym.conc.ch6.completable;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-
 public class CFutureMain7 {
 
     public static Integer calc(Integer para) {
         return para / 2;
     }
 
-    public static void main(String[] args) {
-        CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-            }
-            return calc(50);
-
-        }).orTimeout(1, TimeUnit.SECONDS).exceptionally(e -> {
-            System.err.println(e);
-            return 0;
-        }).thenAccept(System.out::println);
-
-        try
-
-        {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-        }
-    }
+    //public static void main(String[] args) {
+    //    CompletableFuture.supplyAsync(() -> {
+    //        try {
+    //            Thread.sleep(2000);
+    //        } catch (InterruptedException e) {
+    //        }
+    //        return calc(50);
+    //
+    //    }).orTimeout(1, TimeUnit.SECONDS).exceptionally(e -> {
+    //        System.err.println(e);
+    //        return 0;
+    //    }).thenAccept(System.out::println);
+    //
+    //    try
+    //
+    //    {
+    //        Thread.sleep(2000);
+    //    } catch (InterruptedException e) {
+    //    }
+    //}
 }
